2455812bc93579b794a745e7ab8fe32fbb535ddc
Dadu2122 <sharmatatu355@gmail.com>
2026-08-17 22:42:22 +0530

    Update CoverScreen.kt

--- app/src/main/java/com/shreeyog/engteck/screens/CoverScreen.kt
+++ app/src/main/java/com/shreeyog/engteck/screens/CoverScreen.kt
@@ -1,8 +1,8 @@
-import androidx.compose.foundation.clickable
 package com.shreeyog.engteck.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
