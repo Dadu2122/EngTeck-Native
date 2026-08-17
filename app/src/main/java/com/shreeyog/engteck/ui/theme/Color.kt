7f857413f9f56b33dfdc22a8d573fffccecaa27e
Dadu2122 <sharmatatu355@gmail.com>
2026-08-17 12:13:54 +0530

    Update Color.kt

--- app/src/main/java/com/shreeyog/engteck/ui/theme/Color.kt
+++ app/src/main/java/com/shreeyog/engteck/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.shreeyog.engteck.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val EngTeckNavy = Color(0xFF0D1B3E)
-val EngTeckGold = Color(0xFFD4A017)
-val EngTeckWhite = Color(0xFFFFFFFF)
+val NavyDeep = Color(0xFF0D1B3E)
+val NavyMid = Color(0xFF1A2F5C)
+val Gold = Color(0xFFD4A017)
+val InkSoft = Color(0xFF6B7280)
+val Paper = Color(0xFFF7F7F9)
