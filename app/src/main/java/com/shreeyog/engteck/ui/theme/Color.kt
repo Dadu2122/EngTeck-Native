4903403a3b0087ed2f3b269baffd8e4c52b191c6
Dadu2122 <sharmatatu355@gmail.com>
2026-08-17 12:10:51 +0530

    Create Color.kt

---
+++ app/src/main/java/com/shreeyog/engteck/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.shreeyog.engteck.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val EngTeckNavy = Color(0xFF0D1B3E)
+val EngTeckGold = Color(0xFFD4A017)
+val EngTeckWhite = Color(0xFFFFFFFF)
